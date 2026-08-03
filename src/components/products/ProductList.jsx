46c237dbd9b414b0d2858eea995a3da62a44e542
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-03 23:50:38 +0300

    Completed product list component

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
+import { index } from "../../services/productService";
+import { Link } from "react-router";
+
 function ProductList() {
-    return(
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchAllProducts = async () => {
+            try {
+                const products = await index();
+                setProducts(products);
+            } catch (e) {
+                console.log("Error: ", e.message)
+            }
+        }
+
+        fetchAllProducts();
+    }, []);
+
+    return (
         <>
             <h1>All Products</h1>
+            {products.map(product => (
+                <div key={product._id}>
+                    <Link to={`/products/${product._id}`}>
+                        <h3>{product.title}</h3>
+                    </Link>
+                </div>
+            ))}
         </>
     )
 }
